c4936011a9304b47656c69fcccb13a51df7d5f71
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2018-09-24 15:52:42 -0400

    create and validate

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,9 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
+
+use Illuminate\Http\Request;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -21,9 +23,37 @@ Route::get('/', function () {
     // return view('welcome')->with('links', $links);
     // // dynamic method to name the variable
     // return view('welcome')->withLinks($links);
-    return view('welcome',['links' => $links]);
+    return view('welcome', ['links' => $links]);
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/submit', function () {
+    return view('submit');
+});
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    // use without tap
+    // $link = new \App\Link($data);
+    // $link->save();
+    // return $link;
+
+    // use without fillable
+    // $link = new \App\Link;
+    // $link->title = $data['title'];
+    // $link->url = $data['url'];
+    // $link->description = $data['description'];
+    // $link->save();
+    
+    $link = tap(new App\Link($data))->save();
+
+    return redirect('/');
+});
