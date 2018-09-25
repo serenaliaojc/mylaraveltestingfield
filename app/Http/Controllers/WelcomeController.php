d2973e1f92d40fb5c7fb1f6cade3b0ab57d95b33
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2018-09-25 15:30:33 -0400

    move functions into controller

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    //
+    function index() {
+        $links = \App\Link::all();
+        // // with()
+        // return view('welcome')->with('links', $links);
+        // // dynamic method to name the variable
+        // return view('welcome')->withLinks($links);
+        return view('welcome', ['links' => $links]);
+    }
 }
