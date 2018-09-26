4868301253a1d2df4a5f23909fa6cb1a9655c21c
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2018-09-26 14:14:23 -0400

    controller bound with model

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Event;
+use Illuminate\Http\Request;
 
 class EventsController extends Controller
 {
@@ -14,10 +14,7 @@ class EventsController extends Controller
      */
     public function index()
     {
-        // $events = Event::paginate(10);
-        $events = Event::simplePaginate(10);
-
-        return view('events.index')->with('events',$events); 
+        //
     }
 
     /**
@@ -44,21 +41,21 @@ class EventsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Event  $event
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Event $event)
     {
-        //
+        return view('events.show')->with('event', $event);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\Event  $event
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Event $event)
     {
         //
     }
@@ -67,10 +64,10 @@ class EventsController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\Event  $event
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Event $event)
     {
         //
     }
@@ -78,10 +75,10 @@ class EventsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Event  $event
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Event $event)
     {
         //
     }
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -1,6 +1,24 @@
-{{-- Output the $id variable. --}}
+@extends('layouts.test')
+
+@section('content')
+
+    @if ($event)
+        <h1>{{ $event->name }}</h1>
+        <div>
+            <p>City: {{ $event->city }}</p>
+            <p>Venue: {{ $event->venue }}</p>
+            <p>Description: {{ $event->description }}</p>
+        </div>
+
+    @else
+        <h1>Not found.</h1>
+    @endif
+    
+@endsection
+
+{{-- Output the $id variable.
 <p>We're looking at event ID #{{ $id }}.</p>
 <p>{{ $name }} has the id #{{ $id }}.</p>
 <p>{{ $name }} is scheduled for {{ $date }}!</p>
 <h1>Welcome, {{ $name or 'HackerPair Member' }}!</h1>
-<!-- <p>{!! 'My list <script>alert("spam spam spam!")</script>' !!}</p> -->
+<!-- <p>{!! 'My list <script>alert("spam spam spam!")</script>' !!}</p> --> --}}
