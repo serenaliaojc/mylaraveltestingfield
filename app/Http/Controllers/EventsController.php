82bf408827d4623888435445aa9b601c3fcb2062
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2018-09-27 14:53:18 -0400

    create events

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -30,7 +30,7 @@ class EventsController extends Controller
      */
     public function create()
     {
-        //
+        return view('events.create');
     }
 
     /**
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -19,9 +19,11 @@
     
 @endsection
 
+{{-- <p>{{ 'My list <script>alert("spam spam spam!")</script>' }}</p> --}}
+
 {{-- Output the $id variable.
 <p>We're looking at event ID #{{ $id }}.</p>
 <p>{{ $name }} has the id #{{ $id }}.</p>
 <p>{{ $name }} is scheduled for {{ $date }}!</p>
 <h1>Welcome, {{ $name or 'HackerPair Member' }}!</h1>
-<!-- <p>{!! 'My list <script>alert("spam spam spam!")</script>' !!}</p> --> --}}
+ --}}
